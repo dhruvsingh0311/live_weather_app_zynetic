638e7e3192c049749b80e39613673ad860da9a81
dhruvsingh0311 <dhruvsingh123agmail.com>
2025-04-09 04:01:12 +0530

    Your commit message

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "3207c342a38014cefd25b1fd05f86e85",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
