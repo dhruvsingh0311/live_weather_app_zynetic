e0afcaf66b59c74d9f23c1a9674eed540b422937
dhruvsingh0311 <dhruvsingh123agmail.com>
2025-04-09 04:37:17 +0530

    Build production with fixed Netlify redirects

---
+++ build/precache-manifest.57d363ef49b05d2eb4099b9c2085490b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563edecf7b0db958618c0058562f7637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9106084275e05d7acfa8",
+    "url": "/static/css/main.7474ace7.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9106084275e05d7acfa8",
+    "url": "/static/js/main.9947e8c9.chunk.js"
+  },
+  {
+    "revision": "30259e7a30d4e8b4ab16",
+    "url": "/static/js/runtime-main.a9559881.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/static/media/city.e1698332.jpg"
+  }
+]);
